6fc16be6e49e9988e8d69ec7cc7bf87341f49c4e
Svitlana Grytsai <34807666+SvitlanaG@users.noreply.github.com>
2021-03-06 14:09:00 +0100

    feat: implement towelSort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,22 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    let towelSortedArray = [];
+    let openBrackets;
+  if (typeof matrix == 'undefined' || matrix.length == 0) return towelSortedArray;
+  else {
+      for (let i = 0; i < matrix.length; i++) {
+          if (i % 2 === 0) {
+            let temp = matrix.slice(i, i + 1);
+            openBrackets = temp[0];
+            towelSortedArray.push(...openBrackets);
+          } else {
+            let temp = matrix.slice(i, i + 1);
+            temp[0].sort((a, b) =>  b - a);
+            openBrackets = temp[0];
+            towelSortedArray.push(...openBrackets);
+          }
+      }
+  }
+  return towelSortedArray;
 }
